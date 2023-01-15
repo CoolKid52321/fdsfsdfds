3e497121c5867046f8d8cb9514519fd2697356a9
CoolKid52321 <119928093+CoolKid52321@users.noreply.github.com>
2023-01-15 02:01:56 -0500

    Add files via upload

---
+++ dsadasdsa.lua
@@ -0,0 +1,29 @@
+MODS = {
+    [1336782997] = true; -- imagine
+}
+
+ADMIN = {
+    [320265573] = true; -- N_WK
+    [2660709258] = true; -- dual
+}
+
+--// Don't mess with anything below this
+function initiateNames()
+    game.Players.LocalPlayer.Character.UpperTorso:FindFirstChild('OriginalSize'):Destroy()
+    for _,v in pairs(game:GetService('Players'):GetPlayers()) do
+        if v.Character then
+            if not v.Character.UpperTorso:FindFirstChild('OriginalSize') then
+                v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😎] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
+            end
+            if MODS[v.UserId] then
+                v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
+            end
+            if ADMIN[v.UserId] then
+                v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
+            end
+        end
+    end
+end
+
+local successful, errored = pcall(initiateNames)
+return MODS
